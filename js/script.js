c946abf947ead1b039c536eddd25a5569be05e1d
Kyrylo Tsybulskyi <k.tsybulskyi@gmail.com>
2020-12-19 23:02:14 +0100

    Added generated list of tags at the right column

--- js/script.js
+++ js/script.js
@@ -334,7 +334,7 @@ function authorClickHandler(event){
   console.log('authorLinks = ', authorLinks);
   
   /* find all author links with "href" attribute equal to the "href" constant */
-  
+
   const activeLinks = clickedElement.querySelectorAll('a[href^="by ' + href + '"]');
 
   /* START LOOP: for each found author link */
@@ -376,3 +376,101 @@ function addClickListenersToAuthors(){
 }
 
 addClickListenersToAuthors();
+
+const optTagsListSelector = '.tags.list';
+
+function generateTags(){
+
+  /* [NEW] create a new variable allTags with an empty object */
+
+  let allTags = {};
+
+  /* find all articles */
+
+  const articles = document.querySelectorAll(optArticleSelector);
+  console.log('articles = ', articles);
+
+  /* START LOOP: for every article: */
+
+  for(let article of articles){
+    article.querySelector(optArticleSelector);
+    console.log('article = ', article);
+
+    /* find tags wrapper */
+
+    const tagsList = article.querySelector(optArticleTagsSelector);
+    console.log('tagsList = ', tagsList);
+
+    /* make html variable with empty string */
+
+    let html = '';
+
+    /* get tags from data-tags attribute */
+
+    const articleTags = article.getAttribute('data-tags');
+    console.log('articleTags: ', articleTags);
+
+    /* split tags into array */
+
+    const articleTagsArray = articleTags.split(' ');
+    console.log('articleTagsArray: ', articleTagsArray);
+
+    /* START LOOP: for each tag */
+
+    for(let tag of articleTagsArray){
+      console.log('tag: ',tag);
+
+      /* generate HTML of the link */
+
+      const linkHTML = '<li><a href="#tag-' + tag+'">' + tag + '</a></li>';
+      console.log('linkHTML = ', linkHTML);
+
+      /* add generated code to html variable */
+
+      html = html + linkHTML + ' ';
+      console.log('html = ', html);
+
+      /* [NEW] check if this link is NOT already in allTags */
+      if(!allTags.hasOwnProperty(tag)){
+        /* [NEW] add generated code to allTags objects */
+        allTags[tag] = 1;
+      } else {
+        allTags[tag]++;
+      }
+
+    /* END LOOP: for each tag */
+    
+    }
+
+    /* insert HTML of all the links into the tags wrapper */
+
+    tagsList.innerHTML = html;
+    console.log('tagsList = ', tagsList);
+
+  /* END LOOP: for every article: */
+
+  }
+
+  /* [NEW] find list of tags in right column */
+  const tagList = document.querySelector('.tags');
+
+  /* [NEW] create variable for all links HTML code */
+
+  let allTagsHTML = '';
+
+  /* [NEW] START LOOP: for each tag in allTags: */
+
+  for(let tag in allTags){
+
+    /* [NEW] generate code of a link and add it to allTagsHTML */
+
+    allTagsHTML += tag + ' (' + allTags[tag] + ') ';
+
+  /* [NEW] END LOOP: for each tag in allTags: */
+    
+  }
+    
+  /* [NEW] add html from allTagsHTML to tagLisst */
+
+  tagList.innerHTML = allTagsHTML + '  ';
+}
